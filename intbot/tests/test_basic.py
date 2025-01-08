89a935ae425dc9f31b4724bd69fce4196550c9a8
Artur Czepiel <czepiel.artur@gmail.com>
2025-01-08 14:58:59 +0100

    add basic view/endpoint

--- intbot/tests/test_basic.py
+++ intbot/tests/test_basic.py
@@ -1,7 +1,13 @@
 """
 This file is currently used to test the test harness.
 
-It checks whether the tests are running, can access databse, etc
+It checks whether the tests 
+ * are running
+ * can access databse
+ * can reach the views
+ * can authenticate,
+ * etc
+
 """
 
 from django.contrib.auth.models import User
@@ -14,3 +20,11 @@ def test_database_sanity_check():
 
     assert u.id
     assert u.username == "Poirot"
+
+
+def test_http_sanity_check(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response["Content-Type"] == "application/json"
+    assert response.json()["hello"] == "world"
